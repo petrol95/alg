dbad0e6570d7c3d66dbb5ca1a153a4981cca4d87
Petrol95 <petrol195@yandex.ru>
2018-10-09 01:47:50 +0300

    error fixed

--- BinarySearch.java
+++ BinarySearch.java
@@ -2,36 +2,35 @@
  * Algorithms and data structures. Lesson 1.
  *
  * @author Olga Petrova
- * @version dated Oct 08, 2018
+ * @version dated Oct 09, 2018
  */
 
 // Binary search algorithm
 class BinarySearch {
 
-    public static int search(int arr[], int n) { // returns place of searched item or (-1) otherwise
+    public static void search(int arr[], int n) {
         int low = 0; // first element of checked part
         int high = arr.length - 1; // last element of checked part
         int mid = -1; // middle element
-        int guess = -1; 
         while (low <= high) {
             mid = (low + high) / 2;
             if (arr[mid] == n) {
-                guess = arr[mid]; // found
-                break;
+                System.out.println(n + " is the arr[" + mid + "]"); // found
+                return;
             }
             else if (arr[mid] > n)
                 high = mid - 1; // shift the high bound
             else
                 low = mid + 1; // shift the low bound
         }
-        return guess;
+        System.out.println(n + " wasn't found");
     }
 
     public static void main(String args[]) {
         int arr[] = {1, 3, 5, 7, 9}; // sorted array
         // positive test
-        System.out.println((search(arr, 3) == -1) ? "Item wasn't found" : "Slot " + search(arr, 3));
+        search(arr, 7);
         // negative test
-        System.out.println((search(arr, -1) == -1) ? "Item wasn't found" : "Slot " + search(arr, -1));
+        search(arr, -1);
     }
 }
